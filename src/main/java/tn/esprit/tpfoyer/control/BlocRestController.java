37aca906cf94b0483d1ad05a03da3e15cbc048d3
WafaAlouane <136522765+WafaAlouane@users.noreply.github.com>
2025-04-14 06:34:15 +0100

    Update BlocRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
@@ -1,75 +1,71 @@
 package tn.esprit.tpfoyer.control;
 
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.tpfoyer.entity.Bloc;
-import tn.esprit.tpfoyer.service.IBlocService;
+import tn.esprit.tpfoyer.entity.Etudiant;
+import tn.esprit.tpfoyer.service.IEtudiantService;
 
 import java.util.List;
 
-@Tag(name = "Gestion Bloc pour l'équipe 4DS9")
+
 @RestController
 @AllArgsConstructor
-@RequestMapping("/bloc")
-public class BlocRestController {
+@RequestMapping("/etudiant")
+@CrossOrigin(origins = "http://192.168.50.4:4200")
+public class EtudiantRestController {
 
-    IBlocService blocService;
+    IEtudiantService etudiantService;
 
 
-    //http://localhost:8089/tpfoyer/bloc/retrieve-all-blocs
-
-    @GetMapping("/retrieve-all-blocs")
-    @Operation(description = "WS de récuperation de tous les Blocs ")
-    public List<Bloc> getBlocs() {
-        return blocService.retrieveAllBlocs();
-        //return listBlocs;
+    @GetMapping("/retrieve-all-etudiants")
+    public List<Etudiant> getEtudiants() {
+        List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
+        return listEtudiants;
     }
 
 
-    // http://localhost:8089/tpfoyer/bloc/retrieve-bloc/8
-    @GetMapping("/retrieve-bloc/{bloc-id}")
-    public Bloc retrieveBloc(@PathVariable("bloc-id") Long bId) {
-        Bloc bloc = blocService.retrieveBloc(bId);
-        return bloc;
 
+
+
+
+
+
+
+
+
+
+    @GetMapping("/retrieve-etudiant-cin/{cin}")
+    public Etudiant retrieveEtudiantParCin(@PathVariable("cin") Long cin) {
+        Etudiant etudiant = etudiantService.recupererEtudiantParCin(cin);
+        return etudiant;
     }
 
-    // http://localhost:8089/tpfoyer/bloc/add-bloc
-    @PostMapping("/add-bloc")
-    public Bloc addBloc(@RequestBody Bloc c) {
-        Bloc bloc = blocService.addBloc(c);
-        return bloc;
+
+    @GetMapping("/retrieve-etudiant/{etudiant-id}")
+    public Etudiant retrieveEtudiant(@PathVariable("etudiant-id") Long chId) {
+        Etudiant etudiant = etudiantService.retrieveEtudiant(chId);
+        return etudiant;
     }
 
-    // http://localhost:8089/tpfoyer/bloc/remove-bloc/{bloc-id}
-    @DeleteMapping("/remove-bloc/{bloc-id}")
-    public void removeBloc(@PathVariable("bloc-id") Long chId) {
-        blocService.removeBloc(chId);
+    // http://localhost:8089/tpfoyer/etudiant/add-etudiant
+    @PostMapping("/add-etudiant")
+    public Etudiant addEtudiant(@RequestBody Etudiant c) {
+        Etudiant etudiant = etudiantService.addEtudiant(c);
+        return etudiant;
     }
 
-    // http://localhost:8089/tpfoyer/bloc/modify-bloc
-    @PutMapping("/modify-bloc")
-    public Bloc modifyBloc(@RequestBody Bloc c) {
-        Bloc bloc =blocService.modifyBloc(c);
-        return bloc;
+    // http://localhost:8089/tpfoyer/etudiant/remove-etudiant/{etudiant-id}
+    @DeleteMapping("/remove-etudiant/{etudiant-id}")
+    public void removeEtudiant(@PathVariable("etudiant-id") Long chId) {
+        etudiantService.removeEtudiant(chId);
     }
 
-    @GetMapping("/trouver-blocs-sans-foyer")
-    public List<Bloc> getBlocswirhoutFoyer() {
-        List<Bloc> listBlocs = blocService.trouverBlocsSansFoyer();
-        return listBlocs;
+    // http://localhost:8089/tpfoyer/etudiant/modify-etudiant
+    @PutMapping("/modify-etudiant")
+    public Etudiant modifyEtudiant(@RequestBody Etudiant c) {
+        Etudiant etudiant = etudiantService.modifyEtudiant(c);
+        return etudiant;
     }
 
-    @GetMapping("/get-bloc-nb-c/{nb}/{c}")
-    public List<Bloc> recuperBlocsParNomEtCap(
-            @PathVariable("nb") String nb,
-            @PathVariable("c") long c) {
-
-        return blocService.trouverBlocsParNomEtCap(nb, c);
-
-    }
 
 }
